5652df492c34061f25bc97f9f72d0cc73a3ac3d0
Adrielle Alves <adriellesantosalves@hotmail.com>
2020-10-25 15:55:41 -0300

    Add vector-post figure and home-button on the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    home
+    <figure class="home-image-container">
+      <img class="home-image" src="../assets/images/Vetor-posts.png" alt="Vector-posts">
+    </figure>
+
+    <a class="home-button" href="#">Agendar post</a>
   </div>
 </template>
 
@@ -11,3 +15,37 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+
+.home-image-container {
+  margin-top: 85px;
+  margin-bottom: 50px;
+}
+
+.home-image {
+  width: 100%;
+}
+
+.home-button {
+  display: block;
+  text-decoration: none;
+  color: #FFF;
+  background-color: #F2994A;
+  padding-top: 10px;
+  padding-right: 30px;
+  padding-bottom: 10px;
+  padding-left: 30px;
+  text-align: center;
+  border: 1px solid #E0812C;
+  border-radius: 4px;
+  margin: auto;
+}
+</style>
