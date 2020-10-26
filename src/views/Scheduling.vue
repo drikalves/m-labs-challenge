d70a8b94022c18eadbaaa9583db1fb72d64136cc
Adrielle Alves <adriellesantosalves@hotmail.com>
2020-10-26 00:46:48 -0300

    Create schedule-form and the schedule post button modifier

--- src/views/Scheduling.vue
+++ src/views/Scheduling.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="scheduling">
+    <form class="schedule-form">
+      <div class="card">Redes Sociais</div>
+      <div class="card">Data da publicação</div>
+      <div class="card">Texto do post</div>
+      <div class="card">Upload de imagem</div>
+
+      <button class="button button--large button--secondary button--fullwidth" href="">
+        Vizualizar post
+      </button>
+    </form>
     <Footer />
   </div>
 </template>
@@ -16,4 +26,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.schedule-form {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: center;
+  padding: 17px 30px;
+}
+
+.card {
+  background-color: #FFF;
+  border: 1px solid #E0E0E0;
+  border-radius: 4px;
+  padding: 18px;
+  margin-bottom: 15px;
+  width: 100%;
+}
 </style>
