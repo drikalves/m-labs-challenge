0f50579ac03b20e8401f7d0e3ef1ab9dc5ee7f37
Adrielle Alves <adriellesantosalves@hotmail.com>
2020-10-27 01:52:20 -0300

    Add input-field to the post-datetime card

--- src/views/Scheduling.vue
+++ src/views/Scheduling.vue
@@ -9,10 +9,10 @@
         <div class="card-content social-networks">
           <i class="fab fa-instagram"></i>
           <i class="fab fa-linkedin-in"></i>
-          <i class="fab fa-youtube"></i>
-          <i class="fab fa-pinterest"></i>
-          <i class="fab fa-twitter"></i>
-          <i class="fab fa-facebook-f"></i>
+          <i class="fab fa-youtube fab--is-disabled"></i>
+          <i class="fab fa-pinterest fab--is-disabled"></i>
+          <i class="fab fa-twitter fab--is-disabled"></i>
+          <i class="fab fa-facebook-f fab--is-disabled"></i>
         </div>
       </div>
 
@@ -21,7 +21,17 @@
           >Data de publicação
         </div>
 
-        <div class="card-content"></div>
+        <div class="card-content post-datetime">
+          <div class="input-container">
+            <i class="fas fa-calendar-alt icon"></i>
+            <input class="input-field" type="text" placeholder="DD/MM/AAAA">
+          </div>
+
+          <div class="input-container">
+            <i class="fas fa-clock icon"></i>
+            <input class="input-field" type="text" placeholder="HH:MM">
+          </div>
+        </div>
       </div>
 
       <div class="card">
@@ -59,29 +69,40 @@ export default {
 }
 
 .schedule-form {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  flex-direction: column;
   align-items: center;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
   padding: 17px 30px;
+  width: 100%;
 }
 
 .social-networks {
   justify-content: space-between;
 }
 
+.post-datetime {
+  justify-content: space-between;
+}
+
+.post-datetime > .input-container {
+  width: 45%;
+}
+
 .fab {
   background-color: white;
   border: 1px solid #989898;
-  padding: 8px;
+  padding: 8px;.post-datetime > .input-container:first-of-type {
+  margin-right: 15px;
+}
   width: 33px;
   text-align: center;
   text-decoration: none;
   border-radius: 50%;
 
   &:hover {
+    background-color: #FFF3E0;
     border-color: #EF9F2E;
     color: #EF9F2E;
   }
@@ -93,7 +114,7 @@ export default {
 
 .fab--is-disabled {
   background-color: #DDDDDD;
-  border-color: #989898;
+  border-color: #9E9E9E;
   color: #989898;
   pointer-events: none;
 
