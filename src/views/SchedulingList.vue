24a8e77ad472e61d25e4f54a2e26ea14b29cd184
Adrielle Alves <adriellesantosalves@hotmail.com>
2020-10-29 20:49:13 -0300

    Add text to the status column of the scheduling list page

--- src/views/SchedulingList.vue
+++ src/views/SchedulingList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page-scheduling-list">
+  <div class="scheduling-list">
     <table class="m-table">
       <tr>
         <th>Redes Sociais</th>
@@ -12,17 +12,17 @@
 
       <tr>
         <td>
-          <i class="page-scheduling-list__icon
-            page-schedule-list__icon--instagram
+          <i class="scheduling-list__icon
+            schedule-list__icon--instagram
             fab
             fa-instagram">
           </i>
         </td>
         <td>
-          <img class="page-scheduling-list__img" src="../assets/images/photo1.jpg" alt="" />
+          <img class="scheduling-list__midia" src="../assets/images/photo1.jpg" alt="" />
         </td>
         <td>
-          <p>Aqui vai o texto descritivo desse post</p>
+          <p class="scheduling-list__text">Aqui vai o texto descritivo desse post</p>
         </td>
         <td>
           <p>09/09/2020 às 14:45h</p>
@@ -31,22 +31,26 @@
           <a href="">Preview</a>
         </td>
         <td>
-          <div class="page-scheduling-list__status
-            page-scheduling-list__status--is-scheduled"></div>
+          <div class="scheduling-list__status">
+            <div class="scheduling-list__status-badge
+            scheduling-list__status-badge--is-scheduled">
+            </div>
+            <p>Agendado</p>
+          </div>
         </td>
       </tr>
       <tr>
         <td>
-          <i class="page-scheduling-list__icon
-            page-schedule-list__icon--linkedin-in
+          <i class="scheduling-list__icon
+            schedule-list__icon--linkedin-in
             fab fa-linkedin-in"></i>
         </td>
         <td>
-          <img class="page-scheduling-list__img" src="../assets/images/photo2.jpg" alt="" />
+          <img class="scheduling-list__midia" src="../assets/images/photo2.jpg" alt="" />
         </td>
         <td>
-          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-           officia consequat duis enim velit mollit.</p>
+          <p class="scheduling-list__text">Amet minim mollit non deserunt ullamco est sit
+            aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
         </td>
         <td>
           <p>09/09/2020 às 14:45h</p>
@@ -55,8 +59,12 @@
           <a href="">Preview</a>
         </td>
         <td>
-          <div class="page-scheduling-list__status
-            page-scheduling-list__status--is-posted"></div>
+          <div class="scheduling-list__status">
+            <div class="scheduling-list__status-badge
+            scheduling-list__status-badge--is-posted">
+            </div>
+            <p>Postado</p>
+          </div>
         </td>
       </tr>
     </table>
@@ -71,7 +79,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page-scheduling-list {
+.scheduling-list {
   max-width: 100%;
   overflow: auto;
   padding-bottom: 80px;
@@ -80,51 +88,64 @@ export default {
   padding-top: 80px;
   white-space: nowrap;
 
-  .page-scheduling-list__icon {
+  .scheduling-list__icon {
     align-items: center;
     border-radius: 50%;
     color: rgb(255, 255, 255);
-    display: flex;
+    display: flex;align-items: center;
     font-size: 18px;
     height: 32px;
     justify-content: center;
     width: 32px;
 
-    &.page-schedule-list__icon--instagram {
+    &.schedule-list__icon--instagram {
       background-color: rgb(214, 15, 134);
     }
 
-    &.page-schedule-list__icon--linkedin-in {
+    &.schedule-list__icon--linkedin-in {
       background-color: rgb(58, 112, 218);
     }
   }
 
-  .page-scheduling-list__img {
+  .scheduling-list__midia {
     height: 50px;
     width: 50px;
   }
 
-  .page-scheduling-list__status {
+  .scheduling-list__text {
+    max-width: 600px;
+    min-width: 400px;
+    text-align: left;
+    white-space: normal;
+  }
+
+  .scheduling-list__status {
     align-items: center;
-    border-radius: 50%;
     display: flex;
-    height: 12px;
-    width: 12px;
+    justify-content: flex-start;
 
-    &.page-scheduling-list__status--is-scheduled{
-      background-color: rgb(47, 128, 237);
-    }
+    .scheduling-list__status-badge {
+      border-radius: 50%;
+      display: flex;
+      height: 12px;
+      margin-right: 6px;
+      width: 12px;
 
-    &.page-scheduling-list__status--is-posted{
-      background-color: rgb(39, 174, 96);
-    }
+      &.scheduling-list__status-badge--is-scheduled{
+        background-color: rgb(47, 128, 237);
+      }
 
-    &.page-scheduling-list__status--is-posted-with-reservations{
-      background-color: rgb(242, 201, 76);
-    }
+      &.scheduling-list__status-badge--is-posted{
+        background-color: rgb(39, 174, 96);
+      }
 
-    &.page-scheduling-list__status--not-approved{
-      background-color: rgb(235, 87, 87);
+      &.scheduling-list__status--is-parcial-posted{
+        background-color: rgb(242, 201, 76);
+      }
+
+      &.scheduling-list__status--not-approved{
+        background-color: rgb(235, 87, 87);
+      }
     }
   }
 }
